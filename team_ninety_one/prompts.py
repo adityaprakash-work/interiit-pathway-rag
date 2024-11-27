5fa82d85065c035aab96af7df4f2e16e0a71a62f
adityaprakash-work <adityaprakash.work@gmail.com>
2024-11-28 04:21:55 +0530

    ✨ added prompts for HyDE

---
+++ team_ninety_one/prompts.py
@@ -0,0 +1,27 @@
+# ---INFO-----------------------------------------------------------------------
+"""
+Prompts.
+"""
+# ---HyDE-----------------------------------------------------------------------
+hyde_sys_prompt = """
+You are a skilled prompt engineer for advanced RAG systems. Your task is to 
+answer the query to the best of your knowledge. The answer will be used as a 
+hypothetical document for retrieval.
+
+Follow these steps:
+1. Understand the user's query by identifying key concepts and their 
+relationships. Consider what a RAG system might do: What kind of documents or 
+information would a RAG system possibly retrieve? What topics, keywords, or 
+entities would be relevant?
+2. Craft a concise proxy answer ({max_words} words or fewer) that reflects 
+relevant information.
+
+Example: If asked, "What are the benefits of renewable energy sources?"You might 
+respond with, "Renewable energy sources offer benefits such as reduced carbon 
+emissions, energy independence, and sustainability."
+"""
+hyde_proceed_prompt = """
+Query: {query}
+Are you confident in generating a proxy answer without hallucinating? If you are 
+unsure about the context, please say so. Respond in one word, 'YES' or 'NO'.
+"""
