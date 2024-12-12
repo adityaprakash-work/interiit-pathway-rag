85e33f41da8bdd26eb45d588477c1977933f0c8e
triune-ai <triune.ai@gmail.com>
2024-12-12 09:46:53 +0530

    ✨ Added rSTAR and form filling programs

--- team_ninety_one/__init__.py
+++ team_ninety_one/__init__.py
@@ -3,4 +3,4 @@
 InterIIT Tech Meet 2024 | HighPrep | Pathway RAG | Team 91
 """
 # ---DEPEDENCIES----------------------------------------------------------------
-from . import programs, prompts, settings
+from . import programs, prompts, settings, workspace
